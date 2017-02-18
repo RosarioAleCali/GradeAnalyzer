aa0fcb0a89a8c18165f6aca92f9bdff116554a32
RosarioAleCali <racali@myseneca.ca>
2017-02-17 21:51:27 -0500

    Update GradeAnalyzer.c

--- GradeAnalyzer.c
+++ GradeAnalyzer.c
@@ -1,11 +1,3 @@
-/*
-   I declare that the attached assignment is wholly my
-   own work in accordance with Seneca Academic Policy. No part of this
-   assignment has been copied manually or electronically from any
-   other source (including web sites) or distributed to other students.
-   Name Rosario A. Cali Student ID 022567150
-*/
-
 //Include necessary libraries
 #include <stdio.h>
 #include <math.h> // new
